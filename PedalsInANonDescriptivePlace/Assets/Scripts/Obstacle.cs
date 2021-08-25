a953177380b5b3fd2a05e622397840ff5596270d
Diego de Palacio <diego@playpulse.com>
2021-08-25 18:21:32 +0200

    First version of Gameplay items scripts. Removed the Messenger that will not be used at the end.

---
+++ PedalsInANonDescriptivePlace/Assets/Scripts/Obstacle.cs
@@ -0,0 +1,36 @@
+using System;
+using UnityEngine;
+
+namespace PedalsInANonDescriptivePlace
+{
+    [RequireComponent(typeof(Collider))]
+    public class Obstacle : MonoBehaviour
+    {
+        [SerializeField] private float _collisionDamage;
+        [SerializeField] private float _frictionDamage;
+
+        private Seagull _collidingSeagull;
+
+        private void OnCollisionEnter(Collision other)
+        {
+            if (other.gameObject.CompareTag("Seagull"))
+            {
+                _collidingSeagull = other.gameObject.GetComponent<Seagull>();
+                _collidingSeagull.OnColliding(_collisionDamage);
+            }
+        }
+
+        private void OnCollisionExit(Collision other)
+        {
+            if (other.gameObject == _collidingSeagull.gameObject)
+            {
+                _collidingSeagull = null;
+            }
+        }
+
+        private void FixedUpdate()
+        {
+            _collidingSeagull?.OnColliding(_frictionDamage);
+        }
+    }
+}

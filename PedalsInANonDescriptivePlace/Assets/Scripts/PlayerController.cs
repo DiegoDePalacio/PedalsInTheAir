504aa661fcbf64df840d16469f8315fbcc59d772
Diego de Palacio <diego@playpulse.com>
2021-08-26 09:47:33 +0200

    Updated steering by using the right input from the bike.

--- PedalsInANonDescriptivePlace/Assets/Scripts/PlayerController.cs
+++ PedalsInANonDescriptivePlace/Assets/Scripts/PlayerController.cs
@@ -2,7 +2,7 @@
 
 namespace PedalsInANonDescriptivePlace
 {
-    [RequireComponent(typeof(Rigidbody))]    
+    [RequireComponent(typeof(Rigidbody))]
     public class PlayerController : MonoBehaviour
     {
         private const string PEDAL_AXIS = "PedalInput";
@@ -10,8 +10,24 @@ namespace PedalsInANonDescriptivePlace
         private const string BRAKE = "Fire2";
         private const string ACCELERATE = "Fire3";
 
-        private const float EPSILON = 0f;
-        
+        public static KeyCode steerLeftKeyboard = KeyCode.A;
+        public static KeyCode steerRightKeyboard = KeyCode.D;
+        public static KeyCode ability1Keyboard = KeyCode.Q;
+        public static KeyCode ability2Keyboard = KeyCode.E;
+        public static KeyCode ability3Keyboard = KeyCode.Space;
+        public static KeyCode ability4Keyboard = KeyCode.R;
+        public static KeyCode startLeftKeyboard = KeyCode.T;
+        public static KeyCode startRightKeyboard = KeyCode.Y;
+
+        public static KeyCode joy0 = KeyCode.JoystickButton1;
+        public static KeyCode joy1 = KeyCode.JoystickButton0;
+        public static KeyCode joy2 = KeyCode.JoystickButton2;
+        public static KeyCode joy3 = KeyCode.JoystickButton3;
+        public static KeyCode joy4 = KeyCode.JoystickButton4;
+        public static KeyCode joy5 = KeyCode.JoystickButton5;
+        public static KeyCode joy6 = KeyCode.JoystickButton6;
+        public static KeyCode joy7 = KeyCode.JoystickButton7;
+
         [SerializeField] private Vector3 _pedalForce = 5f * Vector3.up;
         [SerializeField] private Vector3 _steeringSpeed = 1f * Vector3.up;
         [SerializeField] private float _maxPedalVelocity = 4f;
@@ -24,6 +40,9 @@ namespace PedalsInANonDescriptivePlace
         private Rigidbody _rigidbody;
         private bool isSoaring;
 
+        private static bool SteerRight => Input.GetKey(steerRightKeyboard) || Input.GetKey(joy3);
+        private static bool SteerLeft => Input.GetKey(steerLeftKeyboard) || Input.GetKey(joy2);
+        
         private void Awake()
         {
             _rigidbody = GetComponent<Rigidbody>();
@@ -76,10 +95,23 @@ namespace PedalsInANonDescriptivePlace
 
         private void UpdateSteering()
         {
-            float steer = Input.GetAxis(HORIZONTAL_STEER);
-            transform.Rotate(_steeringSpeed * steer);
-        }
+            // float steer = Input.GetAxis(HORIZONTAL_STEER);
+            // transform.Rotate(_steeringSpeed * steer);
+            
+            float steering = (Input.GetAxis("SteerLeft") - Input.GetAxis("SteerRight"));
 
+#if (UNITY_EDITOR_WIN || UNITY_STANDALONE_WIN)
+            steering *= 0.5f;
+#endif
+
+            if (SteerLeft)
+                steering += 1f;
+            if (SteerRight)
+                steering -= 1f;
+            
+            transform.Rotate(_steeringSpeed * steering);
+        }
+        
         private void UpdateForwardVelocity()
         {
             float verticalVelocity = _rigidbody.velocity.y;
